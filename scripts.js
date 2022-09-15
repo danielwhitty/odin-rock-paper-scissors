e0df3080db009a7b3e5452d34cdc6f5bb1892f0a
Daniel Whitty <d.james.whitty@gmail.com>
2022-09-15 22:56:54 +1000

    Add base functions to game

---
+++ scripts.js
@@ -0,0 +1,41 @@
+// Array of possible choices
+const CHOICES = ['Rock', 'Paper', 'Scissors'];
+
+// Gets random choice for computer player
+function getComputerChoice() {
+    return CHOICES[Math.floor(Math.random() * 3)];
+}
+
+// Function to ensure user input matches capitalisation of CHOICES array
+function capitalFirstLetter(word) {
+    return word.slice(0, 1).toUpperCase() 
+        + word.slice(1, word.length).toLowerCase();
+}
+
+// Get input from player
+let playerSelection = capitalFirstLetter(prompt("Rock, paper, or scissors?"));
+
+// Plays a single game of Rock Paper Scissors
+function playRound(playerSelection, computerSelection) {
+    let winMessage = 'You win!';
+    let lossMessage = 'You lose!';
+    let tieMessage = 'You tie!';
+    
+    switch (playerSelection) {
+        case 'Rock':
+            computerSelection === 'Paper' ? console.log(lossMessage) :
+                computerSelection === 'Scissors' ? console.log(winMessage) :
+                console.log(tieMessage);
+                break;
+        case 'Paper':
+            computerSelection === 'Scissors' ? console.log(lossMessage) :
+                computerSelection === 'Rock' ? console.log(winMessage) :
+                console.log(tieMessage);
+                break;
+        case 'Scissors':
+            computerSelection === 'Rock' ? console.log(lossMessage) :
+                computerSelection === 'Paper' ? console.log(winMessage) :
+                console.log(tieMessage);
+                break;
+    }
+}
